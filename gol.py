70fe712c1e6f08995fc27d8c7eec39c78dd1bfd0
Lewis <lewis@labaker.co.uk>
2017-09-30 15:21:51 +0100

    Add run_sim

--- gol.py
+++ gol.py
@@ -1,9 +1,31 @@
+from random import randint
 from pprint import pprint
 
+
 class Gol:
+    game_board = []
 
-    def __init__(self):
-        pass
+    def __init__(self, x, duration=None):
+        self.x = x
+        self.duration = duration
+        self.game_board = self._build_board()
 
-    def board(self):
-        pass
+    def _build_board(self):
+        matrix = [[0 for i in range(self.x)] for i in range(self.x)]
+        return matrix
+
+    def set_start_position(self):
+        row = randint(0,9)
+        col = randint(0,9)
+        self.game_board[row][col] = 1
+
+    def run_sim(self):
+        run_num = 0
+        while run_num < self.duration:
+            print("Run: {}".format(run_num + 1))
+            pprint(self.game_board)
+            run_num += 1
+
+gol = Gol(10, 3)
+gol.set_start_position()
+gol.run_sim()

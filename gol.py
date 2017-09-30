02393a1bb761845fce8429c29541ab3f346940f2
Lewis <lewis@labaker.co.uk>
2017-09-30 15:23:36 +0100

    Create private method

--- gol.py
+++ gol.py
@@ -8,7 +8,7 @@ class Gol:
     def __init__(self, x, duration=None):
         self.x = x
         self.duration = duration
-        self.game_board = self._build_board()
+        self._game_board = self._build_board()
 
     def _build_board(self):
         matrix = [[0 for i in range(self.x)] for i in range(self.x)]
@@ -17,13 +17,13 @@ class Gol:
     def set_start_position(self):
         row = randint(0,9)
         col = randint(0,9)
-        self.game_board[row][col] = 1
+        self._game_board[row][col] = 1
 
     def run_sim(self):
         run_num = 0
         while run_num < self.duration:
             print("Run: {}".format(run_num + 1))
-            pprint(self.game_board)
+            pprint(self._game_board)
             run_num += 1
 
 gol = Gol(10, 3)
